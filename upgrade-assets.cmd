ed3589de87225ca8e7925c11501c10ebe711568e
Ryan McQuen <rpcm@linux.com>
2021-05-09 22:44:14 -0700

    Gotta figure out this rollup thingy.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

---
+++ upgrade-assets.cmd
@@ -0,0 +1,4 @@
+curl https://cdn.jsdelivr.net/npm/codemirror@5/lib/codemirror.min.js -o assets/codemirror.min.js
+curl https://cdn.jsdelivr.net/npm/codemirror@5/addon/mode/simple.min.js -o assets/simple.min.js
+curl https://cdn.jsdelivr.net/gh/mickael-kerjean/codemirror-orgmode/orgmode-fold.min.js -o assets/orgmode-fold.min.js
+curl https://cdn.jsdelivr.net/gh/mickael-kerjean/codemirror-orgmode/orgmode-mode.min.js -o assets/orgmode-mode.min.js
