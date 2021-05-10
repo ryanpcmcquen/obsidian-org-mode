ba39a13e2aa3821e85783abbf127acfd3404508e
Ryan McQuen <rpcm@linux.com>
2021-05-10 15:13:59 -0700

    Something is jacked with the build system ...
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- upgrade-assets.cmd
+++ upgrade-assets.cmd
@@ -1,3 +1,3 @@
-curl https://cdn.jsdelivr.net/npm/codemirror@5/addon/mode/simple.min.js -o assets/mode/simple/simple.min.js
-curl https://cdn.jsdelivr.net/gh/mickael-kerjean/codemirror-orgmode/orgmode-fold.min.js -o assets/mode/orgmode/orgmode-fold.min.js
-curl https://cdn.jsdelivr.net/gh/mickael-kerjean/codemirror-orgmode/orgmode-mode.min.js -o assets/mode/orgmode/orgmode-mode.min.js
+curl https://cdn.jsdelivr.net/npm/codemirror@5/addon/mode/simple.min.js -o src/mode/simple/simple.min.js
+curl https://cdn.jsdelivr.net/gh/mickael-kerjean/codemirror-orgmode/orgmode-fold.min.js -o src/mode/orgmode/orgmode-fold.min.js
+curl https://cdn.jsdelivr.net/gh/mickael-kerjean/codemirror-orgmode/orgmode-mode.min.js -o src/mode/orgmode/orgmode-mode.min.js
