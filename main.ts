2b6e789bd6c02bbc3fd84814bd635734aa8a4bd0
Ryan McQuen <rpcm@linux.com>
2021-05-10 14:15:58 -0700

    Reorganize external libs to match ini plugin for Obsidian.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
+import './assets/lib/codemirror.js';
+import './assets/mode/simple/simple.min.js';
+import './assets/mode/orgmode/orgmode-fold.min.js';
+import './assets/mode/orgmode/orgmode-mode.min.js';
 import { Plugin } from 'obsidian';
 
 export default class OrgMode extends Plugin {
-	async onload() {
-		super.onload();
+    async onload() {
+        super.onload();
 
-		// Register the view and extensions:
-		this.registerExtensions(['org'], 'orgmode');
-	}
+        // Register the view and extensions:
+        this.registerExtensions(['org'], 'orgmode');
+    }
 }
--- rollup.config.js
+++ rollup.config.js
@@ -11,13 +11,7 @@ if you want to view the source visit the plugins github repository
 `;
 
 export default {
-  input: [
-    'assets/codemirror.min.js',
-    'assets/simple.min.js',
-    'assets/orgmode-fold.min.js',
-    'assets/orgmode-mode.min.js',
-    'main.ts',
-  ],
+  input: 'main.ts',
   output: {
     dir: '.',
     sourcemap: 'inline',
