f46c452ae44e80b504210d34fd48d1877a9783f2
Ryan McQuen <rpcm@linux.com>
2021-05-11 10:20:44 -0700

    It works! So many thank yous to @deathau.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,29 @@ import './lib/codemirror.js';
 import './mode/simple/simple.min.js';
 import './mode/orgmode/orgmode-fold.min.js';
 import './mode/orgmode/orgmode-mode.min.js';
-import { Plugin, WorkspaceLeaf, addIcon, TextFileView } from 'obsidian';
+import { Plugin, WorkspaceLeaf, TextFileView } from 'obsidian';
 
 export default class OrgMode extends Plugin {
-    settings: any;
-
     async onload() {
         super.onload();
         console.log('Loading Org Mode plugin ...');
 
-        this.settings = (await this.loadData()) || ({} as any);
-
-        // register the view and extensions
         this.registerView('orgmode', this.orgViewCreator);
         this.registerExtensions(['org'], 'orgmode');
     }
 
-    // function to create the view
     orgViewCreator = (leaf: WorkspaceLeaf) => {
         return new OrgView(leaf);
     };
+
+    onunload() {
+        console.log('Unloading Org Mode plugin ...');
+    }
 }
 
-// This is the custom view
+// This is the custom view:
 class OrgView extends TextFileView {
-    // internal code mirror instance
+    // Internal code mirror instance:
     codeMirror: CodeMirror.Editor;
 
     // this.contentEl is not exposed, so cheat a bit.
@@ -35,75 +33,59 @@ class OrgView extends TextFileView {
         return this.contentEl;
     }
 
-    // constructor
     constructor(leaf: WorkspaceLeaf) {
         super(leaf);
-        console.log(this, leaf);
-        // create code mirror instance
         // @ts-ignore
         this.codeMirror = CodeMirror(this.extContentEl, {
             theme: 'obsidian',
         });
-        // register the changes event
+
         this.codeMirror.on('changes', this.changed);
     }
 
-    // when the view is resized, refresh CodeMirror (thanks Licat!)
+    // When the view is resized, refresh CodeMirror (thanks Licat!).
     onResize() {
         this.codeMirror.refresh();
     }
 
-    // called on code mirror changes
     changed = async (
         instance: CodeMirror.Editor,
         changes: CodeMirror.EditorChangeLinkedList[]
     ) => {
-        // request a debounced save in 2 seconds from now
         this.requestSave();
     };
 
-    // get the new file contents
     getViewData = () => {
-        console.log(this);
         return this.codeMirror.getValue();
     };
 
-    // set the file contents
     setViewData = (data: string, clear: boolean) => {
-        console.log('file data: ', data);
         if (clear) {
             // @ts-ignore
-            this.codeMirror.swapDoc(CodeMirror.Doc(data, 'text/plain'));
+            this.codeMirror.swapDoc(CodeMirror.Doc(data, 'orgmode'));
         } else {
             this.codeMirror.setValue(data);
         }
     };
 
-    // clear the view content
     clear = () => {
         this.codeMirror.setValue('');
         this.codeMirror.clearHistory();
     };
 
-    // gets the title of the document
     getDisplayText() {
-        console.log(this);
-        if (this.file) return this.file.basename;
-        else return 'org (no file)';
+        if (this.file) {
+            return this.file.basename;
+        } else {
+            return 'org (no file)';
+        }
     }
 
-    // confirms this view can accept org extension
     canAcceptExtension(extension: string) {
         return extension === 'org';
     }
 
-    // the view type name
     getViewType() {
         return 'orgmode';
     }
-
-    // icon for the view
-    // getIcon() {
-    //     return 'document-org';
-    // }
 }
